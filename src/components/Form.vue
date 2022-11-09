23eeaf7a3ef03031a68ed0706e6e8686ff6a554f
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-11-09 14:07:25 -0300

    finalizando projeto

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,44 +2,74 @@
   <div>
     <h2>Consulte o seu endereço com o seu CEP</h2>
     <p>Não utilize nº de casa/ apto/ lote/ <br>prédio ou abreviatura</p>
-    <div class="frm-row">
-      <label for="cep">CEP</label>
-      <input />
-    </div>
+    <form>
+      <div class="frm-row">
+          <label for="cep">CEP</label>
+          <input type="text"  v-model="cep"  v-on:change="consulta_cep" id="cep" maxlength="8" >
+      </div>
+      <!--<div class="frm-row">
+          <label for="rua">Rua</label>
+          <input type="text" id="rua" > 
+          {{ cep_data.logradouro}}
+      </div>
+      <div class="frm-row">
+          <label for="bairro">Bairro</label>
+          <input type="text" id="bairro" >
+      </div>  
+      <div class="frm-row">
+          <label for="cidade">Cidade</label>   
+          <input type="text" id="cidade"> 
+          
+      </div>  
+      <div class="frm-row">
+          <label for="estado">Estado</label>
+          <input type="text"  id="estado" >
+      </div>!-->
 
-    <div class="frm-row">
-      <label for="cep">Rua</label>
-      <input />
-    </div>
-
-    <div class="frm-row">
-      <label for="cep">Bairro</label>
-      <input />
-    </div>  
-
-    <div class="frm-row">
-      <label for="cep">Cidade</label>
-      <input />
-    </div>  
-
-    <div class="frm-row">
-      <label for="cep">Estado</label>
-      <input />
-    </div>
-
+      <div v-if="cep_data != null">
+        Logradouro: {{cep_data.logradouro}}
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import { assertExpressionStatement } from '@babel/types';
 
-import input from './input.vue';
 
 export default {
   name: 'Form',
-  components:{
-    input
+  data(){
+
+    return{
+      cep: "",
+      cep_data: null
+    }
+  },
+  methods:{
+    consulta_cep(){
+      var self = this;
+
+      axios.get('https://viacep.com.br/ws/'+this.cep+'/json/')
+      .then(function (response) {
+
+        console.log(response);
+        self.cep_data = response.data;
+
+      })
+      .catch(function (error){
+
+        //console.log(error);
+      })
+      .finally(function () {
+
+      });
+    }
   }
 }
+
+
+
 </script>
 
 <style scoped>
@@ -60,4 +90,15 @@ label{
     font-family: arial;
     color:rgb(30, 134, 231)
   }
+
+  input{
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #fff;
+    display: block;
+    background-color: rgb(206, 205, 205);
+    width: calc(100% - 110px);
+    margin-left: 4%;
+    margin-bottom: 15px;
+}
 </style>
